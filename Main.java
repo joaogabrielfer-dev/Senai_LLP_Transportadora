9bada0505022c0e8b7290f3bcab7f65c2d37ee9f
Elratodicapri0 <cangvagaga@gmal.com>
2026-05-17 11:46:09 -0300

    Adição de funcionalidades que havia esquecido

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-import java.nio.channels.Pipe.SourceChannel;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,17 +7,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         
-        String receita, passo;
+        String receita, passo, ingredient, quantidade;
         ArrayList<String> passoapasso = new ArrayList<>();
-        int contador = 1;
+        ArrayList<String> ingredientes = new ArrayList<>();
+        ArrayList<String> quantidades = new ArrayList<>();
+        int contador = 1, contador2 = 1;
 
 
         System.out.println("Digite o nome de Sua receita: ");
         receita = ler.nextLine();
 
         while (true) {
+            System.out.printf("\n\nDigite o %d ingrediente de sua receita: ", contador2);
+            System.out.println("\nCaso Deseje finalizar a adição de ingredientes não digite nada e aperte ENTER.");
+            ingredient = ler.nextLine();
+            if (ingredient.isEmpty()) {
+                break;
+            }
+            ingredientes.add(ingredient);
+
+            System.out.print("Quantidade: ");
+            quantidade = ler.nextLine();
+            quantidades.add(quantidade);
+
+            contador2++;
+        }
+
+        while (true) {
             System.out.printf("%d passo: ", contador);
-            System.out.println("\nCaso Deseje finalizar a receita não digite nada e aperte ENTER.");
+            System.out.println("\nCaso Deseje finalizar a adição de passos não digite nada e aperte ENTER.");
             passo = ler.nextLine();
 
             if(passo.isEmpty()){
@@ -28,10 +47,52 @@ public class Main {
         }
 
         System.out.println(receita);
+        System.out.println("\nPIngredientes");
+
+        for(int i = 0; i < ingredientes.size(); i++ ){
+            System.out.println((i +1) + ". " + ingredientes.get(i) + " - " + quantidades.get(i));
+        }
         System.out.println("\nPasso a passo da receita:");
+
         for(int i = 0; i < passoapasso.size(); i++ ){
             System.out.println((i +1) + ". " + passoapasso.get(i));
         }
+
+        try {
+
+            FileWriter arquivo = new FileWriter(receita + ".txt");
+
+            arquivo.write(receita + "\n\n");
+
+            arquivo.write("Ingredientes:\n");
+
+            for(int i = 0; i < ingredientes.size(); i++ ) {
+
+                arquivo.write((i +1) + ". "
+                + ingredientes.get(i)
+                + " - "
+                + quantidades.get(i)
+                + "\n");
+            }
+
+            arquivo.write("\nModo de Preparo:\n");
+
+            for(int i = 0; i < passoapasso.size(); i++ ) {
+
+                arquivo.write((i +1) + ". "
+                + passoapasso.get(i)
+                + "\n");
+            }
+
+            arquivo.close();
+
+            System.out.println("Arquivo salvo");
+
+        } catch(IOException e) {
+
+            System.out.println("Erro ao salvar arquivo.");
+        }            
+
 
         ler.close();
     }
